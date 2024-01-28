d4fcc7387134497d3bd78dc34e82d68a594b07c0
Greekzombie <pabloiyu@hotmail.es>
2024-01-28 14:32:54 +0000

    adapter

--- adapter/llm_train.py
+++ adapter/llm_train.py
@@ -1,31 +1,17 @@
-"""
-Note: For it to work on WSL2 I followed
-
-https://docs.nvidia.com/cuda/wsl-user-guide/index.html
-
-Had to uninstall sudo rm cuda-repo-wsl-ubuntu-12-3-local_12.3.1-1_amd64.deb
-Must have had an inproper installation
-
-Also had to update PATH variables
-export PATH=/usr/local/cuda-12.3/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64:$LD_LIBRARY_PATH
-"""
-
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from tqdm import tqdm
-from mamba_ssm import Mamba
 import sys
 
 #hyperparams
 lr = 1e-3
 batch_size = 32
-block_size = 512
+block_size = 256
 device = "cuda" if torch.cuda.is_available() else "cpu"
 if device != "cuda":
     sys.exit(0)
-max_iters = 801
+max_iters = 1001
 print_iters = 100
 eval_iters = 10
 eval_interval = 100
@@ -80,46 +66,106 @@ def estimate_loss(eval_iters, train_data, val_data):
         out[split] = losses.mean()
     model.train()
     return out
+  
+  
+class Adapter(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Adapter, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
 
+        self.adapter = nn.Sequential(
+            nn.Linear(self.input_size, self.hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size, self.output_size),
+        )
+
+    def forward(self, x):
+        return self.adapter(x)
+  
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.key = nn.Linear(n_embed, head_size, bias=False)
+        self.query = nn.Linear(n_embed, head_size, bias=False)
+        self.value = nn.Linear(n_embed, head_size, bias=False)
+        
+        self.dropout = nn.Dropout(dropout)
+        self.head_size = head_size
+
+    def forward(self, x):
+        _,T,_ = x.shape
+        K = self.key(x)   
+        Q = self.query(x) 
+        
+        # Einsum is easier way to view the batched matrix multiplication. 
+        wei = torch.einsum('ijk, ilk -> ijl', Q, K) * self.head_size**(-0.5)
+        
+        # We now mask values so that decoder can't look at future tokens
+        wei = wei.masked_fill(self.tril[:T,:T]==0, float('-inf')) 
+        wei = F.softmax(wei, dim=-1) 
+        wei = self.dropout(wei)
+        
+        # We then incorporate the value matrix
+        V = self.value(x) 
+        out = wei @ V 
+        return out
+
+
+class MultiHead(nn.Module):
+    def __init__(self, n_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(n_heads)])
+        self.linear = nn.Linear(n_heads * head_size, n_embed)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        x = torch.cat([h(x) for h in self.heads], dim=-1)
+        x = self.dropout(self.linear(x))
+        return x
 
 class FeedForward(nn.Module):
-  def __init__(self, n_embed) -> None:
-    super().__init__()
-    self.ffn = nn.Sequential(
-      nn.Linear(n_embed, 4*n_embed),
-      nn.ReLU(),
-      nn.Linear(4*n_embed, n_embed),
-      nn.Dropout(dropout),
-    ).to(device)
-  def forward(self, x):
-    return self.ffn(x)
+    def __init__(self, n_embed) -> None:
+        super().__init__()
+        self.ffn = nn.Sequential(
+        nn.Linear(n_embed, 4*n_embed),
+        nn.ReLU(),
+        nn.Linear(4*n_embed, n_embed),
+        nn.Dropout(dropout),
+        ).to(device)
+        
+    def forward(self, x):
+        return self.ffn(x)
 
 class Block(nn.Module):
   def __init__(self, n_embed, n_heads) -> None:
     super().__init__()
     self.head_size = n_embed // n_heads
-    self.mamba_head = Mamba(
-      d_model=n_embed, # dimension of model
-      d_state=16,     # SSM state expansion factor
-      d_conv=4,      # local convolution width
-      expand=2,    # block expansion factor
-  ).to(device)
-    self.ln1 = nn.LayerNorm(n_embed).to(device)
+    self.sa_head = MultiHead(n_heads, self.head_size)
+    self.ln1 = nn.LayerNorm(n_embed)
+    self.ln2 = nn.LayerNorm(n_embed)
+    self.feedforward = FeedForward(n_embed)
+    self.adapter1 = Adapter(n_embed, n_embed//8, n_embed)  # Define adapter with appropriate dimensions
+    self.adapter2 = Adapter(n_embed, n_embed//8, n_embed)
 
   def forward(self, x):
-    # We use norm before inputting into
-    x = x + self.mamba_head(self.ln1(x))
-    
+    # We use norm before
+    # Parallel adapters are useful when dealing with large pre-trained models
+    x = x + self.sa_head(self.ln1(x)) + self.adapter1(self.ln1(x))
+    x = x + self.feedforward(self.ln2(x)) + self.adapter2(self.ln2(x))
+
     return x
 
 class LLM(nn.Module):
   def __init__(self,vocab_size):
     super().__init__()
-    self.token_embedding_table = nn.Embedding(vocab_size,n_embed).to(device)
-    self.position_embedding_table = nn.Embedding(block_size,n_embed).to(device)
-    self.lm_head = nn.Linear(n_embed,vocab_size).to(device)
-    self.blocks = nn.Sequential(*[Block(n_embed,n_heads=n_heads) for _ in range(n_layers)]).to(device)
-    self.ln_f = nn.LayerNorm(n_embed).to(device) # final layer norm
+    self.token_embedding_table = nn.Embedding(vocab_size,n_embed)
+    self.position_embedding_table = nn.Embedding(block_size,n_embed)
+    self.lm_head = nn.Linear(n_embed,vocab_size)
+    self.blocks = nn.Sequential(*[Block(n_embed,n_heads=n_heads) for _ in range(n_layers)])
+    self.ln_f = nn.LayerNorm(n_embed)# final layer norm
 
   def forward(self, idx, targets=None):
     # idx = idx[:,-block_size:]
